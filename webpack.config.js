24f15dbddd3aa621fa9960428393f99d49ebebda
Siegerus <ivanoffsiegerus@gmail.com>
2025-06-25 19:44:57 +0300

    1

--- webpack.config.js
+++ webpack.config.js
@@ -6,23 +6,23 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist/js')
   },
-  module: {
-    rules: [
-      {
-        test: /\.(?:js|mjs|cjs)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            targets: "defaults",
-            presets: [
-              ['@babel/preset-env']
-            ]
-          }
-        }
-      }
-    ]
-  },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.(?:js|mjs|cjs)$/,
+  //       exclude: /node_modules/,
+  //       use: {
+  //         loader: 'babel-loader',
+  //         options: {
+  //           targets: "defaults",
+  //           presets: [
+  //             ['@babel/preset-env']
+  //           ]
+  //         }
+  //       }
+  //     }
+  //   ]
+  // },
    mode: 'development'
 };
 
